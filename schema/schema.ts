e907de1ccd89900ed16235b1b9bdd68326f1aec3
SamuelGustafsson <samuelgustafsson890623@gmail.com>
2017-12-08 16:17:07 +0100

    One way relationship between comments and user.

--- schema/schema.ts
+++ schema/schema.ts
@@ -2,12 +2,29 @@ import * as graphql from "graphql";
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 const axios = require("axios");
 
+const CommentType = new GraphQLObjectType({
+  name: "Comment",
+  fields: {
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    comment: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstname: { type: GraphQLString },
-    lastname: { type: GraphQLString }
+    lastname: { type: GraphQLString },
+    comment: {
+      type: CommentType,
+      resolve(_parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/comments/${_parentValue.commentId}`)
+          .then(response => response.data);
+      }
+    }
   }
 });
 
@@ -18,10 +35,18 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(_parentValue: any, args: any) {
-        console.log(args.id);
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
           .then(response => response.data);
+      }
+    },
+    comment: {
+      type: CommentType,
+      args: { id: { type: GraphQLString } },
+      resolve(_parentValue, args) {
+        return axios(`http://localhost:3000/comments/${args.id}`).then(
+          response => response.data
+        );
       }
     }
   }
@@ -30,3 +55,5 @@ const RootQuery = new GraphQLObjectType({
 module.exports = new GraphQLSchema({
   query: RootQuery
 });
+
+// En commentar ska ha en relation till en vändare och en nyhet.
